a2e8b50ef88826619a06d06ac45b66606b456003
Guilherme <111164179+guirque@users.noreply.github.com>
2022-11-02 22:09:50 -0300

    Novas funções e correção de lcreate
    Funções adicionadas:
    - lerase;
    - lfind;
    - lprint;
    Funções modificadas:
    - lcreate: agora, requer valor como argumento para inseri-lo na lista criada.

--- lista-encadeada.c
+++ lista-encadeada.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 
 //Definicao
-//llist*: ponteiro para elemento da lista (sao o L e os ponteiro dentro da lista)
-//llist: um elemento da lista
 typedef struct linked_list
 {
     int value;
@@ -22,20 +20,43 @@ void laddToStart(llist** aList, int add)
 
     newElement->next = *aList; //Novo elemento aponta para o segundo elemento (que antes era o primeiro)
     *aList = newElement; //Ponteiro apontado por aList (inicio da cadeia) comeca a apontar para esse novo elemento
-    
-    //printf("Elemento adicionado: %d\n", (**aList).value);
-    //printf("Seu próximo é: %d\n", (**aList).next->value);
 }
 
-llist* lcreate()
+//Apaga um elemento numa posicao da lista
+void lerase(llist** aList, int index)
+{
+    llist* before = NULL;
+    llist* current = *aList;
+    for(int i = 0; i <= index; i++)
+    {
+        //Encontrado
+        if(i == index)
+        {
+            if(index != 0) before->next = current->next; //Mudando o ponteiro do anterior para o posterior
+            else *aList = current->next; //Caso o elemento seja o primeiro da lista, mudamos a quem aponta o ponteiro inicial
+            free(current); //Apagando o atual da memoria
+        }
+        //Nao Encontrado
+        else
+        {
+            before = current;
+            current = current->next;
+        }
+    }
+
+}
+
+//Cria uma lista com elemento inicial de valor especificado
+llist* lcreate(int add)
 {
     //Cria o ponteiro para o primeiro elemento da lista encadeada
     llist* l = (llist*) malloc(sizeof(llist));
+    l->value = add;
+    l->next = NULL;
     return l;
-} //ok
+}
 
-//Usar ponteiro de ponteiro permite que se pegue seu conteudo (primeiro elemento) assim como modifique a quem aponta.
-//Usar ponteiro permite que se pegue o primeiro elemento (apenas)
+//Retorna o valor do elemento na posicao index
 int lget(llist* aList, int index)
 {
     for(int i = 0; i <= index; i++)
@@ -43,20 +64,61 @@ int lget(llist* aList, int index)
         if(i == index) return aList->value;
         else aList = aList->next;
     }
-} //ok
+}
+
+//Retorna o indice do primeiro elemento de valor value. -1 caso nao exista.
+int lfind(llist* aList, int value)
+{
+    for(int i = 0; aList != NULL; i++)
+    {
+        if(aList->value == value) return i;
+        else aList = aList->next;
+    }
+    return -1;
+}
+
+//Imprime a lista
+void lprint(llist* aList)
+{
+    printf("{");
+    for(int i = 0; aList != NULL; i++)
+    {
+        if(aList->next != NULL)
+            printf("%d, ", aList->value);
+        else
+            printf("%d", aList->value);
+        aList = aList->next;
+    }
+    printf("}");
+}
 
 int main()
 {
     llist* umaLista; //Criando o ponteiro da lista
-    umaLista = lcreate(); //Criando a lista em si
+    umaLista = lcreate(15); //Criando a lista em si
     
     //Adicionando elementos
-    laddToStart(&umaLista, 10); 
+    laddToStart(&umaLista, 10);
     laddToStart(&umaLista, 255);
     laddToStart(&umaLista, 87);
-
+    laddToStart(&umaLista, 255);
+    
+    //Imprimindo a lista
+    lprint(umaLista);
+    
+    //Apagando elementos da lista
+    lerase(&umaLista, 0);
+    lerase(&umaLista, 2);
+    
+    //Imprimindo apos apagar elementos
+    printf("\n");
+    lprint(umaLista);
+    
     //Retornando um elemento a partir de um indice
-    printf("\nResultado de lget: %d\n", lget(umaLista, 0));
+    printf("\nResultado de lget: %d", lget(umaLista, 1));
+    
+    //Encontrando o indice do primeiro elemento com valor 255
+    printf("\nIndice do primeiro elemento com valor 255: %d\n", lfind(umaLista, 255));
     
     return 0;
 }
